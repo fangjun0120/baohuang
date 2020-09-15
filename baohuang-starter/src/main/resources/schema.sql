59eb52164d8fa492e6c5a4e9adf5e16bf2f541eb
jfang <jfang0120@zhejianglab.com>
2020-09-15 17:23:49 +0800

    almost done

---
+++ baohuang-starter/src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `game` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_time` bigint DEFAULT NULL,
+  `updated_time` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `user` (
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `created_time` bigint DEFAULT NULL,
+                        `updated_time` bigint DEFAULT NULL,
+                        `username` varchar(45) DEFAULT NULL,
+                        `password` varchar(45) DEFAULT NULL,
+                        `portrait` varchar(200) DEFAULT NULL,
+                        `roles` varchar(45) DEFAULT NULL,
+                        PRIMARY KEY (`id`),
+                        UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
